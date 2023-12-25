632ec2401bf477720ca3dd2c3dc5f3ebdcc0d177
m83city <rostorguievv@gmail.com>
2023-12-25 14:58:58 +0200

    crud jook

--- src/main/java/com/example/basejook/api/StudentController.java
+++ src/main/java/com/example/basejook/api/StudentController.java
@@ -3,26 +3,20 @@ package com.example.basejook.api;
 import com.example.basejook.api.dto.StudentDTO;
 import com.example.basejook.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.jooq.DSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import static com.example.basejook.api.mapper.StudentAPIMapper.*;
-import static org.jooq.your.base.Tables.STUDENT;
-
-import org.jooq.your.base.tables.records.StudentRecord;
 
 @RestController
 @RequiredArgsConstructor
 
 public class StudentController {
 
-
     private final StudentService service;
     @GetMapping("/student/{id}")
     public StudentDTO getById (@PathVariable Long id){
         return asStudentDTO(service.getById(id)) ;
     }
-    @PostMapping("/student/")
+    @PostMapping("/student")
     public StudentDTO create (@RequestBody StudentDTO student){
         return asStudentDTO(service.create(asStudent(student)));
     }
--- src/main/java/com/example/basejook/repository/StudentRepository.java
+++ src/main/java/com/example/basejook/repository/StudentRepository.java
@@ -3,9 +3,9 @@ package com.example.basejook.repository;
 import com.example.basejook.service.domain.Student;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.jooq.your.base.tables.records.StudentRecord;
+import java.util.Random;
 
 import static org.jooq.your.base.Tables.STUDENT;
 
@@ -28,10 +28,19 @@ public class StudentRepository {
 
     }
     public Student create (Student student){
-        return null;
+        Random random = new Random();
+        dsl.insertInto(STUDENT, STUDENT.ID, STUDENT.FIRSTNAME, STUDENT.SECONDNAME)
+                .values(random.nextInt(), student.getName(), student.getSurname())
+                .execute();
+        return student;
     }
     public Student update (Long id, Student student){
-        return null;
+        dsl.update(STUDENT)
+                .set(STUDENT.FIRSTNAME, student.getName())
+                .set(STUDENT.SECONDNAME, student.getSurname())
+                .where(STUDENT.ID.eq(id.intValue()))
+                .execute();
+        return student;
     }
     public Long delete (Long id){
         dsl.deleteFrom(STUDENT).where(STUDENT.ID.eq(id.intValue())).execute();
